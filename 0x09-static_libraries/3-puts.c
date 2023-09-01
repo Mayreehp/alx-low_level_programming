ed8db1377eb44a42a873e3a30ca4a749e5c65464
MAYREEHP <mayreehp@gmail.com>
2023-09-01 15:17:54 -0700

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+
+/**
+
+ * _puts - this prints a string on a new line
+
+ * @str: a pointer given by the main function
+
+ * Return: 0
+
+ */
+
+
+
+void _puts(char *str)
+
+{
+
+	int z = 0;
+
+
+
+	while (str[z] != '\0')
+
+
+
+{
+
+	_putchar(str[z]);
+
+	z++;
+
+}
+
+
+
+_putchar(10);
+
+
+
+}
