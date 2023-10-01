2e977d761e918ace5a1ec8821d39c12a3257e80a
Mayreehp <mayreehp@gmail.com>
2023-09-30 17:44:08 -0700

    change

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,16 +1,23 @@
 #include "main.h"
-
 /**
- * get_endianness - checks the endianness
+ * get_endianness - check endianness.
  *
- * Return: 0 if big endian, 1 if little endian
+ * Return: 0 if big endian, or 1 if little endian
  */
+
 int get_endianness(void)
 {
-	int x;
-	char *y;
+	int num = 1;
 
-	x = 1;
-	y = (char *)&x;
-	return (*y);
+	/**
+	  * use a pointer to a char to examine the first byte
+	  * of the memory representation of num.
+	  * This is achieved by casting the address of num
+	  * to a char pointer and then dereferencing it.
+	  */
+
+	if (*(char *)&num == 1)
+		return (1); /* 1 to indicate little-endian */
+	else
+		return (0); /* 0 to indicate big-endian */
 }
