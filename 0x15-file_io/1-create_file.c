5e63932ab136f9f251a22948de0f9fc402e3db7b
Mayreehp <mayreehp@gmail.com>
2023-10-02 09:42:59 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,59 +1,38 @@
 #include "main.h"
 
 /**
- * custom_strlen - calculate length of NULL terminated string
- * @str: string
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: length
+ * Return: 1 if it success. -1 if it fails.
  */
-
-size_t custom_strlen(const char *str)
-{
-	size_t len = 0;
-
-	while (str[len] != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
- * create_file - creates a new file or truncates an existing file,
- * and writes content into it.
- * @filename: name of file to be created.
- * @text_content: NULL terminated string to write to the file.
- *
- * Return: On success, returns 1. On failure or error, returns -1.
- */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd, nb_wrote;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (-1);
-	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
-		return (-1); /* Error while opening/creating file */
+		return (-1);
 
-	/* check if there isn't text to write into the string */
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
+	if (!text_content)
+		text_content = "";
 
-	/* write text into new file */
-	nb_wrote = write(fd, text_content, custom_strlen(text_content));
-	if (nb_wrote == -1)
-	{
-		close(fd);
-		return (-1); /* Error while writing to the file */
-	}
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
+
 	return (1);
 }
